577d413e27ca5d65ea1cb877fa44229084dff94f
gulatnaru <gulatnaru1@naver.com>
2018-06-14 15:36:54 +0900

    20180614 version3

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,9 +3,9 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Test");
 		System.out.println("Test");
+		System.out.println("Test3");
 	}
 
 }
